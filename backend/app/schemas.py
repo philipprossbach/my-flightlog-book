c703f5becaff68f11c81e238ea1a5a4a850a0fcf
philipprossbach <121982043+philipprossbach@users.noreply.github.com>
2025-08-06 22:46:18 +0200

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,25 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
+class FlightBase(BaseModel):
+    departure_airport: str
+    departure_datetime: datetime
+    arrival_airport: str
+    arrival_datetime: datetime
+    flight_number: str
+    flight_type: str  # "privat" oder "geschäftlich"
+    airline: Optional[str] = None
+    seat_number: Optional[str] = None
+    aircraft_registration: Optional[str] = None
+    aircraft_type: Optional[str] = None
+    comment: Optional[str] = None
+
+class FlightCreate(FlightBase):
+    pass
+
+class FlightRead(FlightBase):
+    id: int
+
+    class Config:
+        orm_mode = True
