bd8bdfcf83e7a069b78768b665a5760fbaff9f40
philipprossbach <121982043+philipprossbach@users.noreply.github.com>
2025-08-06 22:51:27 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,3 +6,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://flightuser:flight
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = sessionmaker(engine, autocommit=False, autoflush=False, class_=AsyncSession)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
