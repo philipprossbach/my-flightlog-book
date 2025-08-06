c108ee817f096ee9a61716620804eb4d6817f828
philipprossbach <121982043+philipprossbach@users.noreply.github.com>
2025-08-06 22:28:43 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,8 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://flightuser:flightpass@db/flighttracker")
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+AsyncSessionLocal = sessionmaker(engine, autocommit=False, autoflush=False, class_=AsyncSession)
